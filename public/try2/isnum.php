2601ba920b7a60664c56fea3fa8b9487a5440b96
Gingira1213 <137020136+Gingira1213@users.noreply.github.com>
2025-04-15 02:12:37 +0000

    try2

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -8,10 +8,20 @@
   <body class="cyberpunk-bg">
     <div class="login-box">
       <h2>
-<?php
+      <?php
+      //無理やり配列を送られていないか
+        $foo = filter_input(INPUT_GET, 'indata');
 
+        // 文字列か、空でないかをチェック
+        if (!is_string($foo) || trim($foo) === '') {
+         die("入力してください");
+        }
 
-echo "入力された数字は： " . $_GET['indata'];
+        // mb_ereg数字が入力されているかどうか
+        if (mb_ereg('^[0-9]+$', $foo) === false) {
+          die("数字を入力してください。");
+        }
+        echo "入力された数字は： " . $foo;
 ?>
     </h2>
     </div>
